71d9c5d9732e9041921e924ee7d5f768bba1e570
Thomas Levine <_@thomaslevine.com>
2014-04-25 18:43:14 +0000

    junar api key

--- pluplusch/junar.py
+++ pluplusch/junar.py
@@ -3,3 +3,9 @@ catalogs = [
 
 def download(get, catalog):
     raise NotImplementedError
+
+def apikey(get, catalog, datetime):
+    url = '%s/developer_manager/action_insert?_=%s' % (catalog, datetime.strftime('%s%f')[:-3])
+    response = get(url)
+    data = json.loads(response.text)
+    return data['pApiKey']
