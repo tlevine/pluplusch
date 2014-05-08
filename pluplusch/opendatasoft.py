4875930c66704678d0fcef0b02531eb33e3cf3c8
Thomas Levine <_@thomaslevine.com>
2014-05-08 14:13:11 +0000

    separate schemes

--- pluplusch/opendatasoft.py
+++ pluplusch/opendatasoft.py
@@ -1,18 +1,18 @@
 import json
 
 catalogs = [
-    'http://data.iledefrance.fr',
-    'http://opendata.paris.fr.opendatasoft.com',
-    'http://tourisme04.opendatasoft.com',
-    'http://tourisme62.opendatasoft.com',
-    'http://grandnancy.opendatasoft.com',
-    'http://bistrotdepays.opendatasoft.com',
-    'http://scisf.opendatasoft.com',
-    'http://pod.opendatasoft.com',
-    'http://dataratp.opendatasoft.com',
-    'http://public.opendatasoft.com',
-    'http://ressources.data.sncf.com',
-    'http://data.enseignementsup-recherche.gouv.fr',
+    (('http',), 'data.iledefrance.fr'),
+    (('http',), 'opendata.paris.fr.opendatasoft.com'),
+    (('http',), 'tourisme04.opendatasoft.com'),
+    (('http',), 'tourisme62.opendatasoft.com'),
+    (('http',), 'grandnancy.opendatasoft.com'),
+    (('http',), 'bistrotdepays.opendatasoft.com'),
+    (('http',), 'scisf.opendatasoft.com'),
+    (('http',), 'pod.opendatasoft.com'),
+    (('http',), 'dataratp.opendatasoft.com'),
+    (('http',), 'public.opendatasoft.com'),
+    (('http',), 'ressources.data.sncf.com'),
+    (('http',), 'data.enseignementsup-recherche.gouv.fr'),
 ]
 
 def datasets(get, catalog):
