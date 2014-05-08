da56c02df24691eb303efa10bf959dc9052432dd
Thomas Levine <_@thomaslevine.com>
2014-05-08 14:17:24 +0000

    allow manual software setting

--- pluplusch/index.py
+++ pluplusch/index.py
@@ -8,9 +8,15 @@ def submodules(softwares = softwares):
 def catalogs(submodules = submodules(softwares)):
     for software, module in submodules.items():
         for catalog in getattr(module, 'catalogs'):
-            yield catalog, software
+            schemes, rest_of_url = catalog
+            for scheme in schemes:
+                yield scheme + '://' + rest_of_url, software
 
 def catalog_to_software(catalog, lookup = {}):
+    # If the catalog is an iterable of (catalog, software), just return it.
+    if len(catalog) == 2:
+        return catalog
+
     if lookup == {}:
         lookup.update(catalogs(submodules(softwares)))
 
