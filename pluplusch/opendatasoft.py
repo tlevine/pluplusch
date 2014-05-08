800f23b8424ea1ed3bda40985673e091e61106ac
Thomas Levine <_@thomaslevine.com>
2014-05-08 19:00:51 +0000

    oops

--- pluplusch/opendatasoft.py
+++ pluplusch/opendatasoft.py
@@ -31,7 +31,7 @@ def dataset_download(get, catalog, datasetid):
     url = '%s/explore/dataset/%s/download/?format=csv' % (catalog, datasetid)
     return get(url)
 
-def download(get, catalog, data):
+def download(get, catalog, data, do_standardize):
     '''
     Download everything from an OpenDataSoft catalog.
     '''
