4c516f1177616487083942413653cda16fa6d430
Thomas Levine <_@thomaslevine.com>
2014-05-18 21:26:47 +0000

    socrata tests

--- pluplusch/test/test_socrata.py
+++ pluplusch/test/test_socrata.py
@@ -13,63 +13,8 @@ def test_views_page():
     n.assert_list_equal(observed, [{'abc':'def','catalog':'https://foo.bar',}])
 
 def test_standardize_t6ck_kg3u():
-    original = {
-        'catalog': 'https://healthmeasures.aspe.hhs.gov',
-
-        "id" : "t6ck-kg3u",
-        "name" : "Measure Overviews",
-        "averageRating" : 0,
-        "createdAt" : 1325616190,
-        "displayType" : "table",
-        "downloadCount" : 37,
-        "indexUpdatedAt" : 1397175791,
-        "newBackend" : False,
-        "numberOfComments" : 0,
-        "oid" : 7728928,
-        "publicationAppendEnabled" : False,
-        "publicationDate" : 1397075983,
-        "publicationGroup" : 268458,
-        "publicationStage" : "published",
-        "rowsUpdatedAt" : 1397075953,
-        "rowsUpdatedBy" : "iwgp-sfie",
-        "tableId" : 1540716,
-        "totalTimesRated" : 0,
-        "viewCount" : 643,
-        "viewLastModified" : 1397075983,
-        "viewType" : "tabular",
-        "grants" : [ {
-          "inherited" : True,
-          "type" : "viewer",
-          "flags" : [ "public" ]
-        } ],
-        "metadata" : {
-          "renderTypeConfig" : {
-            "visible" : {
-              "page" : True,
-              "table" : True
-            },
-            "active" : {
-              "page" : {
-                "id" : "40"
-              }
-            }
-          },
-          "availableDisplayTypes" : [ "table", "fatrow", "page" ]
-        },
-        "owner" : {
-          "id" : "dg59-t3xw",
-          "displayName" : "Erin Miller",
-          "screenName" : "Erin Miller"
-        },
-        "rights" : [ "read" ],
-        "tableAuthor" : {
-          "id" : "dg59-t3xw",
-          "displayName" : "Erin Miller",
-          "screenName" : "Erin Miller"
-        },
-        "tags" : [ "measures" ],
-        "flags" : [ "default" ]
-    }
+    with open(os.path.join('pluplusch', 'test', 'fixtures', 't6ck-kg3u.json'), 'r') as fp:
+        original = json.load(fp)
     observed = socrata.standardize(original)
     expected = {
         'url': 'https://healthmeasures.aspe.hhs.gov/d/t6ck-kg3u',
