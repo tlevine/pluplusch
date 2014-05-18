3a5ef76b3c72e1eb3f573efe73486e442b022a31
Thomas Levine <_@thomaslevine.com>
2014-05-18 23:06:45 +0000

    change parallel thingy

--- pluplusch/main.py
+++ pluplusch/main.py
@@ -60,33 +60,42 @@ def pluplusch(catalogs = None,
         catalogs = list(i.all_catalogs())
 
     def dataset_generator(catalog_name, catalog_software, submodules = i.submodules()):
-        for dataset in getattr(submodules[catalog_software], 'metadata')(get, catalog_name):
+        for dataset in submodules[catalog_software].metadata(get, catalog_name):
             if standardize:
-                return getattr(submodules[catalog_software], 'standardize')(get, dataset)
+                yield submodules[catalog_software].standardize(get, dataset)
             else:
-                return dataset
+                yield dataset
 
     catalog_names = ((catalog[0] if len(catalog) == 2 else catalog) for catalog in catalogs)
     catalog_softwares = ((catalog[1] if len(catalog) == 2 else i.catalog_to_software(catalog)) for catalog in catalogs)
     standardized_catalogs = zip(catalog_names, catalog_softwares)
-    generators = {args[0]: dataset_generator(*args) for args in standardized_catalogs}
+    generators = {catalog_name:dataset_generator(catalog_name, catalog_software) for catalog_name, catalog_software in standardized_catalogs}
 
-    while generators != {}:
-        with ThreadPoolExecutor(len(generators)) as e:
-            datasets = e.map(f, generators.values())
-            remove = set()
-            for catalog, dataset in zip(list(generators.keys()), datasets):
-                if dataset == None:
-                    remove.add(catalog)
-                else:
-                    yield dataset
-            for catalog in remove:
-                del(generators[catalog])
+    with ThreadPoolExecutor(len(generators)) as e:
+        from time import sleep
+        queue = []
+        futures = {}
+        while generators != {}:
+            sleep(0.01)
 
-def generate(generator):
-    try:
-        return next(generator)
-    except StopIteration:
-        pass
-    except Exception as e:
-        logger.error(e)
+            # Emit results.
+            yield from queue
+            queue.clear()
+
+            # Start jobs.
+            for catalog_name, generator in generators.items():
+                if catalog_name not in futures:
+                    futures[catalog_name] = e.submit(next, generator)
+
+            # Enqueue results.
+            for catalog_name, future in list(futures.items()):
+                if future.done():
+                    del(futures[catalog_name])
+                    if future.exception() == None:
+                        queue.append(future.result())
+                    elif isinstance(future.exception(), StopIteration):
+                        del(generators[catalog_name])
+                    else:
+                        del(generators[catalog_name])
+                        logger.error('Error on ' + catalog_name)
+                        logger.error(future.exception())
