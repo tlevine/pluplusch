134bfa5b03e9b56ddd7f5ff908085b6800966d49
Thomas Levine <_@thomaslevine.com>
2014-05-23 13:36:43 +0000

    threading

--- pluplusch/main.py
+++ pluplusch/main.py
@@ -17,67 +17,44 @@ def getlogger():
 logger = getlogger()
 
 def pluplusch(get, catalogs = None,
-        cache_dir = os.path.join(os.path.expanduser('~'), '.pluplusch'),
-        proxies = {})
+        cache_dir = os.path.join(os.path.expanduser('~'), '.pluplusch'))
     '''
     pluplusch downloads data from open data websites. Here are
     its inputs.
 
     catalogs
-        List of catalogs do download, each item being either
+        List of catalogs to download, each item being either
         a full URL string, including the scheme, or a tuple
         of the full URL string and the software, in case pluplusch
         doesn't know about the catalog
     cache_dir
         String directory to cache downloads to
-    proxies
-        A dictionary containing any or neither of "http_proxy"
-        and "https_proxy"
 
     It returns a generator of datasets.
     '''
 
+    # Use all catalogs by default.
     if catalogs == None:
-        # Use all catalogs.
         catalogs = list(i.all_catalogs())
 
-    def dataset_generator(catalog_name, catalog_software, submodules = i.submodules()):
-        for dataset in submodules[catalog_software].metadata(get, catalog_name):
-            yield dataset
-
+    # Detect catalog softwares if they're not specified.
     catalog_names = ((catalog[0] if len(catalog) == 2 else catalog) for catalog in catalogs)
     catalog_softwares = ((catalog[1] if len(catalog) == 2 else i.catalog_to_software(catalog)) for catalog in catalogs)
-    standardized_catalogs = zip(catalog_names, catalog_softwares)
-    generators = {catalog_name:dataset_generator(catalog_name, catalog_software) for catalog_name, catalog_software in standardized_catalogs}
+    catalog_names_softwares = zip(catalog_names, catalog_softwares)
 
-    with ThreadPoolExecutor(len(generators)) as e:
-        from time import sleep
-        queue = []
-        futures = {}
-        while generators != {}:
-            sleep(0.01)
-
-            # Emit results.
-            yield from queue
-            queue.clear()
-
-            # Start jobs.
-            for catalog_name, generator in generators.items():
-                if catalog_name not in futures:
-                    futures[catalog_name] = e.submit(next, generator)
-
-            # Enqueue results.
-            for catalog_name, future in list(futures.items()):
-                if future.done():
-                    del(futures[catalog_name])
-                    if future.exception() == None:
-                        queue.append(future.result())
-                    elif isinstance(future.exception(), StopIteration):
-                        del(generators[catalog_name])
-                    else:
-                        del(generators[catalog_name])
-                        logger.error('Error on ' + catalog_name)
-                        logger.error(future.exception())
+    # Threading
+    queue = []
+    running = set(catalog_names)
+    submodules = i.submodules()
+    def enqueue_datasets(catalog_name_software):
+        catalog_name, catalog_software = catalog_name_software
+        for dataset in submodules[catalog_software].metadata(get, catalog_name):
+            queue.append(dataset)
+        running.remove(catalog_name)
+    threaded(catalog_names_softwares, enqueue_datasets)
+    while len(running) > 0:
+        if queue != []:
+            yield queue.pop(0)
 
 def standardize(get, original, download_data = False, csv = False):
 
