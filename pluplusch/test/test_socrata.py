5bc73691a8344a390aefdde9e519baa254f8559a
Thomas Levine <_@thomaslevine.com>
2014-05-27 15:11:12 +0000

    docs

--- pluplusch/test/test_socrata.py
+++ pluplusch/test/test_socrata.py
@@ -15,7 +15,7 @@ def test_views_page():
 def test_standardize_t6ck_kg3u():
     with open(os.path.join('pluplusch', 'test', 'fixtures', 't6ck-kg3u.json'), 'r') as fp:
         original = json.load(fp)
-    observed = socrata._standardize(True, original)
+    observed = socrata.standardize(True, original)
     expected = {
         'url': 'https://healthmeasures.aspe.hhs.gov/d/t6ck-kg3u',
         'download_url': 'https://healthmeasures.aspe.hhs.gov/resource/t6ck-kg3u.csv',
@@ -31,7 +31,7 @@ def test_standardize_t6ck_kg3u():
 def test_standardize_zt9s_n5aj():
     with open(os.path.join('pluplusch', 'test', 'fixtures', 'zt9s-n5aj.json'), 'r') as fp:
         original = json.load(fp)
-    observed = socrata._standardize(True, original)
+    observed = socrata.standardize(True, original)
     expected = {
         'url': 'https://data.cityofnewyork.us/d/zt9s-n5aj',
         'creator_id': 'https://healthmeasures.aspe.hhs.gov/d/5fuc-pqz2',
@@ -49,7 +49,7 @@ def test_standardize_zt9s_n5aj():
 
 def test_standardize_tags():
     original = {'tableAuthor': {'screenName': 'Clare', 'profileImageUrlSmall': '/images/profile/2234/2577/180561_10150096076666249_317976_n_tiny.jpg', 'displayName': 'Clare', 'rights': ['create_datasets', 'edit_others_datasets', 'edit_sdp', 'edit_site_theme', 'moderate_comments', 'manage_users', 'chown_datasets', 'edit_nominations', 'approve_nominations', 'feature_items', 'federations', 'manage_stories', 'manage_approval', 'change_configurations', 'view_domain', 'view_others_datasets', 'edit_pages', 'create_pages', 'view_goals', 'view_dashboards', 'edit_goals', 'edit_dashboards', 'create_dashboards'], 'profileImageUrlMedium': '/images/profile/2234/2577/180561_10150096076666249_317976_n_thumb.jpg', 'profileImageUrlLarge': '/images/profile/2234/2577/180561_10150096076666249_317976_n_large.jpg', 'flags': ['admin'], 'id': 'rqdg-xj2v'}, 'displayType': 'table', 'viewType': 'tabular', 'publicationGroup': 240034, 'newBackend': False, 'publicationDate': 1354232726, 'catalog': 'https://data.undp.org', 'rowsUpdatedBy': 'rqdg-xj2v', 'averageRating': 0, 'viewCount': 167, 'publicationStage': 'published', 'grants': [{'inherited': True, 'flags': ['public'], 'type': 'viewer'}], 'viewLastModified': 1319223058, 'rights': ['read'], 'oid': 1867464, 'numberOfComments': 0, 'rowsUpdatedAt': 1354232632, 'createdAt': 1319223058, 'publicationAppendEnabled': False, 'owner': {'screenName': 'UNDP Transparency', 'displayName': 'UNDP Transparency', 'id': '67n2-h4v3'}, 'indexUpdatedAt': 1397848594, 'moderationStatus': True, 'downloadCount': 67, 'metadata': {'renderTypeConfig': {'visible': {'table': True}}, 'custom_fields': {'Scope': {'Country': 'Bahamas'}}, 'availableDisplayTypes': ['table', 'fatrow', 'page']}, 'name': 'Projects in Bahamas', 'tableId': 527904, 'totalTimesRated': 0, 'id': 'mvag-axbk'}
-    observed = socrata._standardize(True, original)
+    observed = socrata.standardize(True, original)
     expected = {
         'colnames': [],
         'creator_id': 'https://healthmeasures.aspe.hhs.gov/d/67n2-h4v3',
@@ -61,3 +61,9 @@ def test_standardize_tags():
         'url': 'https://data.undp.org/d/mvag-axbk',
     }
     n.assert_dict_equal(observed, expected)
+
+def test_standardize_niuh-hrin():
+    with open(os.path.join('pluplusch', 'test', 'fixtures', 'niuh-hrin.json'), 'r') as fp:
+        original = json.load(fp)
+    observed = socrata.standardize(True, original)
+    expected = {}
