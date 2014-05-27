d8f3de0f71aa7ff6566582bddb38c3a204a1c54e
Thomas Levine <_@thomaslevine.com>
2014-05-27 15:06:31 +0000

    docs

--- pluplusch/cli.py
+++ pluplusch/cli.py
@@ -10,10 +10,14 @@ def arg_parser():
     parser = argparse.ArgumentParser()
 
     cache_dir = os.path.join(os.path.expanduser('~'), '.pluplusch')
-    parser.add_argument('--cache-dir', '-d', metavar = 'DIR', default = cache_dir)
-    parser.add_argument('--download-data', '-D', action = 'store_true', default = False)
-    parser.add_argument('--urls', '-u', action = 'store_true', default = False)
-    parser.add_argument('catalog', metavar = 'CATALOG', nargs = '*')
+    parser.add_argument('--cache-dir', '-d', metavar = 'DIR', default = cache_dir,
+        help = 'Use the specified directory as the cache directory; the default is "%s".' % cache_dir)
+    parser.add_argument('--download-data', '-D', action = 'store_true', default = False,
+        help = 'Set this flag to download full datasets when they are helpful, rather than relying on metadata files.')
+    parser.add_argument('--urls', '-u', action = 'store_true', default = False,
+        help = 'Display just the URLs of downloads rather than the full standardized metadata in jsonlines format.')
+    parser.add_argument('catalog', metavar = 'CATALOG', nargs = '*',
+        help = 'Manually specify catalogs; the default is to use lots of hard-coded catalogs.')
 
     return parser
 
