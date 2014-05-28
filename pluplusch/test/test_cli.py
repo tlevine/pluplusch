ad51c1a19706f8e64d100484d193237849b89b07
Thomas Levine <_@thomaslevine.com>
2014-05-28 20:25:00 +0000

    fix args

--- pluplusch/test/test_cli.py
+++ pluplusch/test/test_cli.py
@@ -9,13 +9,14 @@ class TestArgParser(unittest.TestCase):
     def test_empty(self):
         parsed = self.p.parse_args([])
         n.assert_false(parsed.urls)
-        n.assert_false(parsed.download_data)
+        n.assert_false(parsed.force_colnames)
+        n.assert_false(parsed.silent)
         n.assert_in('.pluplusch', parsed.cache_dir)
         n.assert_list_equal(parsed.catalog, [])
     def test_store_true(self):
-        parsed = self.p.parse_args(['--url', '--download-data'])
+        parsed = self.p.parse_args(['--url', '--force-colnames'])
         n.assert_true(parsed.urls)
-        n.assert_true(parsed.download_data)
+        n.assert_true(parsed.force_colnames)
 
     def test_cache_dir(self):
         parsed = self.p.parse_args(['--cache-dir', 'foo'])
