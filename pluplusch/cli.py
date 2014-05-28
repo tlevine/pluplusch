b65fb1bcaa20f8c544b340608e5c8bc19ecbb47e
Thomas Levine <_@thomaslevine.com>
2014-05-28 19:47:45 +0000

    fix args

--- pluplusch/cli.py
+++ pluplusch/cli.py
@@ -4,7 +4,7 @@ import argparse
 import json
 import sys
 
-from pluplusch.main import pluplusch
+from pluplusch.main import pluplusch, get
 
 def arg_parser():
     parser = argparse.ArgumentParser()
@@ -12,20 +12,27 @@ def arg_parser():
     cache_dir = os.path.join(os.path.expanduser('~'), '.pluplusch')
     parser.add_argument('--cache-dir', '-d', metavar = 'DIR', default = cache_dir,
         help = 'Use the specified directory as the cache directory; the default is "%s".' % cache_dir)
-    parser.add_argument('--download-data', '-D', action = 'store_true', default = False,
+    parser.add_argument('--force-colnames', '-D', action = 'store_true', default = False,
         help = 'Set this flag to download full datasets when they are helpful, rather than relying on metadata files.')
     parser.add_argument('--urls', '-u', action = 'store_true', default = False,
         help = 'Display just the URLs of downloads rather than the full standardized metadata in jsonlines format.')
     parser.add_argument('catalog', metavar = 'CATALOG', nargs = '*',
         help = 'Manually specify catalogs; the default is to use lots of hard-coded catalogs.')
 
+    parser.add_argument('--full', '-f', action = 'store_true', default = False,
+        help = 'Download the full data files rather than just the metadata files.')
+
     return parser
 
 def main(stdout = sys.stdout):
     p = arg_parser().parse_args()
 
-    generator = pluplusch(catalogs = p.catalog, cache_dir = p.cache_dir, standardize = True, download_data = p.download_data)
+    generator = pluplusch(catalogs = p.catalog, standardize = True,
+                          force_colnames = p.force_colnames,
+                          get = lambda url: get(url, cache_dir = p.cache_dir))
     for metadata in generator:
+        if p.full and metadata['download_url'] != None:
+            get(metadata['download_url'])
         if p.urls:
             url = metadata['download_url']
             if url != None:
