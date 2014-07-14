914d64632de89340968eb253a29e71db24b4f453
Thomas Levine <_@thomaslevine.com>
2014-07-14 03:53:07 -0400

    fix unicode

--- pluplusch/test/test_socrata.py
+++ pluplusch/test/test_socrata.py
@@ -11,7 +11,7 @@ import pluplusch.socrata as socrata
 try:
     UTC = datetime.timezone.utc
 except AttributeError:
-    UTC = None # Dates will be interpreted in local time on Python 2.
+    UTC = None
 
 def test_views_page():
     catalog = 'https://foo.bar'
@@ -24,13 +24,13 @@ def test_standardize_t6ck_kg3u():
         original = json.load(fp)
     observed = socrata.standardize(original)
     expected = {
-        u'url': u'https://healthmeasures.aspe.hhs.gov/d/t6ck-kg3u',
-        u'download_url': u'https://healthmeasures.aspe.hhs.gov/resource/t6ck-kg3u.csv',
-        u'title': u'Measure Overviews',
-        u'creator_name': u'Erin Miller',
-        u'creator_id': u'https://healthmeasures.aspe.hhs.gov/d/dg59-t3xw',
-        u'date': datetime.datetime(2014, 4, 9, 20, 39, 43, tzinfo = UTC),
-        u'tags': {u'measures'},
+        'url': 'https://healthmeasures.aspe.hhs.gov/d/t6ck-kg3u',
+        'download_url': 'https://healthmeasures.aspe.hhs.gov/resource/t6ck-kg3u.csv',
+        "title": "Measure Overviews",
+        "creator_name" : "Erin Miller",
+        "creator_id": "https://healthmeasures.aspe.hhs.gov/d/dg59-t3xw",
+        "date": datetime.datetime(2014, 4, 9, 20, 39, 43, tzinfo = UTC),
+        "tags" : {"measures"},
     }
     n.assert_dict_equal(observed, expected)
 
@@ -39,13 +39,13 @@ def test_standardize_zt9s_n5aj():
         original = json.load(fp)
     observed = socrata.standardize(original)
     expected = {
-        u'url': u'https://data.cityofnewyork.us/d/zt9s-n5aj',
-        u'creator_id': u'https://healthmeasures.aspe.hhs.gov/d/5fuc-pqz2',
-        u'creator_name': u'NYC OpenData',
-        u'date': datetime.datetime(2014, 1, 24, 18, 20, 58, tzinfo = UTC),
-        u'download_url': u'https://data.cityofnewyork.us/resource/zt9s-n5aj.csv',
-        u'tags': {u'lifelong learning'},
-        u'title': u'SAT (College Board) 2010 School Level Results',
+        'url': 'https://data.cityofnewyork.us/d/zt9s-n5aj',
+        'creator_id': 'https://healthmeasures.aspe.hhs.gov/d/5fuc-pqz2',
+        'creator_name': 'NYC OpenData',
+        'date': datetime.datetime(2014, 1, 24, 18, 20, 58, tzinfo = UTC),
+        'download_url': 'https://data.cityofnewyork.us/resource/zt9s-n5aj.csv',
+        'tags': {'lifelong learning'},
+        'title': 'SAT (College Board) 2010 School Level Results',
     }
     n.assert_dict_equal(observed, expected)
 
@@ -62,13 +62,13 @@ def test_standardize_tags():
     original = {'tableAuthor': {'screenName': 'Clare', 'profileImageUrlSmall': '/images/profile/2234/2577/180561_10150096076666249_317976_n_tiny.jpg', 'displayName': 'Clare', 'rights': ['create_datasets', 'edit_others_datasets', 'edit_sdp', 'edit_site_theme', 'moderate_comments', 'manage_users', 'chown_datasets', 'edit_nominations', 'approve_nominations', 'feature_items', 'federations', 'manage_stories', 'manage_approval', 'change_configurations', 'view_domain', 'view_others_datasets', 'edit_pages', 'create_pages', 'view_goals', 'view_dashboards', 'edit_goals', 'edit_dashboards', 'create_dashboards'], 'profileImageUrlMedium': '/images/profile/2234/2577/180561_10150096076666249_317976_n_thumb.jpg', 'profileImageUrlLarge': '/images/profile/2234/2577/180561_10150096076666249_317976_n_large.jpg', 'flags': ['admin'], 'id': 'rqdg-xj2v'}, 'displayType': 'table', 'viewType': 'tabular', 'publicationGroup': 240034, 'newBackend': False, 'publicationDate': 1354232726, 'catalog': 'https://data.undp.org', 'rowsUpdatedBy': 'rqdg-xj2v', 'averageRating': 0, 'viewCount': 167, 'publicationStage': 'published', 'grants': [{'inherited': True, 'flags': ['public'], 'type': 'viewer'}], 'viewLastModified': 1319223058, 'rights': ['read'], 'oid': 1867464, 'numberOfComments': 0, 'rowsUpdatedAt': 1354232632, 'createdAt': 1319223058, 'publicationAppendEnabled': False, 'owner': {'screenName': 'UNDP Transparency', 'displayName': 'UNDP Transparency', 'id': '67n2-h4v3'}, 'indexUpdatedAt': 1397848594, 'moderationStatus': True, 'downloadCount': 67, 'metadata': {'renderTypeConfig': {'visible': {'table': True}}, 'custom_fields': {'Scope': {'Country': 'Bahamas'}}, 'availableDisplayTypes': ['table', 'fatrow', 'page']}, 'name': 'Projects in Bahamas', 'tableId': 527904, 'totalTimesRated': 0, 'id': 'mvag-axbk'}
     observed = socrata.standardize(original)
     expected = {
-        u'creator_id': u'https://healthmeasures.aspe.hhs.gov/d/67n2-h4v3',
-        u'creator_name': u'UNDP Transparency',
-        u'date': datetime.datetime(2012, 11, 29, 23, 45, 26, tzinfo = UTC),
-        u'download_url': u'https://data.undp.org/resource/mvag-axbk.csv',
-        u'tags': set(),
-        u'title': u'Projects in Bahamas',
-        u'url': u'https://data.undp.org/d/mvag-axbk',
+        'creator_id': 'https://healthmeasures.aspe.hhs.gov/d/67n2-h4v3',
+        'creator_name': 'UNDP Transparency',
+        'date': datetime.datetime(2012, 11, 29, 23, 45, 26, tzinfo = UTC),
+        'download_url': 'https://data.undp.org/resource/mvag-axbk.csv',
+        'tags': set(),
+        'title': 'Projects in Bahamas',
+        'url': 'https://data.undp.org/d/mvag-axbk',
     }
     n.assert_dict_equal(observed, expected)
 
