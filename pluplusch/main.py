b964cdcaae41f5928cc1cf1334cb00c26132c6e4
Thomas Levine <_@thomaslevine.com>
2014-08-02 15:18:07 +0000

    colnames

--- pluplusch/main.py
+++ pluplusch/main.py
@@ -76,10 +76,7 @@ def _pluplusch(get, catalogs = [], standardize = True, force_colnames = False):
                     out = submodules[catalog_software].standardize(dataset)
                     if out['download_url'] != None:
                         out['download_url'] = urljoin(out['url'], out['download_url'])
-                    if catalog_software == 'ckan' and not force_colnames:
-                        # Getting column names from CKAN involves downloading all the data
-                        out['colnames'] = set()
-                    else:
+                    if force_colnames:
                         out['colnames'] = submodules[catalog_software].colnames(get, dataset)
                 else:
                     out = dataset
